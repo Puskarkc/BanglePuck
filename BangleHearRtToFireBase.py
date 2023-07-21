3b57c1ed3ce511010ff5d03dd098846776edfafd
Puskar K C <puskarkc@gmail.com>
2023-07-21 15:25:19 +1000

    Update BangleHearRtToFireBase.py

--- BangleHearRtToFireBase.py
+++ BangleHearRtToFireBase.py
@@ -1,14 +1,13 @@
-### Code to Start Bangle using Java Script and Post Heart Rate and Temperature in Google Firebase.
-### Author : Puskar K C
-### Python inerfacing code, referenced from Bangle.js official documentatin.
 
+### Code to Start Bangle using Java Script and Post Heart Rate and Temperature in Firebase.
+### Author : Puskar K C
+### Python inerfacing code, referenced from Bangle.js official documentation.
 import asyncio
 import array
 import time
 import pyrebase
 from datetime import datetime
 from bleak import BleakClient
-
 ###  Firebase Config. the configuration details is always different for each Database and user and need to obtained from Firebase
 firebaseConfig = {
   "apiKey": "AIzaSyA8W4ioDBpqh5XC76AwQp_mfnFdbliAY8Y",
@@ -20,17 +19,15 @@ firebaseConfig = {
   "appId": "1:109029043316:web:59f15c494d81c24208b521",
   "measurementId": "G-QEF2F2PHW5"
 }
-
 ### initialize database of Firebase
 firebase = pyrebase.initialize_app(firebaseConfig)
 database = firebase.database()
 ###------------------------- Above code is required for firebase connection
-
-### Bangle.Js Watch Address. The below 3 lines is mandatory code which is just the address of the WATCH
-address = "E43D4199-EB93-DBE8-4565-0E8F0C3202A1"
-UUID_NORDIC_TX = "6e400002-b5a3-f393-e0a9-e50e24dcca9e" ### Bangle Code to Transmit/Push Data
-UUID_NORDIC_RX = "6e400003-b5a3-f393-e0a9-e50e24dcca9e" ### Banlge code or Unique ID to Receive Data.
-
+### Bangle.Js Watch Address. The below 3 lines is mandatory code which is jus the address of the WATCH
+#address = "E43D4199-EB93-DBE8-4565-0E8F0C3202A1" ### Puskar Watch
+address = "5BCD1604-F7C7-4AA1-95D2-3190B2CCE9D3"  ### Alan Watch
+UUID_NORDIC_TX = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+UUID_NORDIC_RX = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
 ###  b"var currentDate = new Date(); print('D'+currentDate.toISOString());"
 #### Using Javascript to pass command to Bangle.Js
 command = (b"\x03\x10 reset()"
@@ -42,20 +39,24 @@ command = (b"\x03\x10 reset()"
           b"Bangle.on('HRM',function(hrm) { "
           b"var d = ['H',hrm.bpm,hrm.confidence]; "
           b"print(temp + d.join());})}, 1000);"
-          b"\n\x10print( 'Fresh Data Start')\n")
+          b"\n\x10 print( 'Fresh Data Start')\n")
 
-### Variables declaration
+
+### The Above code, is the javascript code, Still one problem remeaninf.
+### This codes gives Temperature but for Heart Rate Monitor
 SkinTemperature = None
 HeartData = None
 sample = 1
 person = "Puskar"
 t = time.localtime()
-current_time = time.strftime("%H:%M:%S", t) ### Not in used this for Now -- for time
+current_time = time.strftime("%H:%M:%S", t) ### Not used this for Now, Want to get the Time, you can use this
 sampleCount = 0 ### Variable for counter
 hrtData = None
-def uart_data_received(sender, data):
+async def uart_data_received(sender, data):
   global sampleCount
+  
   received_data = data.decode().strip() ## decode the bytes to string
+  
  # print("Temperature", received_data)
  
   if(received_data.startswith("T")): ## Receiving the line that starts with "T"
@@ -77,7 +78,9 @@ def uart_data_received(sender, data):
       SkinTemperature = {"DataIdentifier": "T", "Temperature ": tempy}
       database.child(person).child("Temperature Reading").child(sampleCount).set(SkinTemperature)
       sampleCount = sampleCount +1
+      await asyncio.sleep(20)
   elif(hrtData.startswith("H")): ## Receiving the line that starts with "H"
+      
       heart= hrtData.split(",")
       if len(heart) >= 3:
         try:
@@ -93,7 +96,6 @@ def uart_data_received(sender, data):
       else:
          print(f'Data not received {heart}')    
 print("Connecting...")
-
 ### from here -Samelessly used sample code from Expurino documentation
 async def run(address, loop):  
     async with BleakClient(address, loop=loop) as client:
@@ -103,7 +105,7 @@ async def run(address, loop):
         
         print("Writing command")
         c = command
-        while len(c) > 0: ### sending 2 bytes as Watch only transmit 20 bytes
+        while len(c) > 0: ### sending 11 bytes as Watch only transmit 20 bytes
             await client.write_gatt_char(UUID_NORDIC_TX, bytearray(c[0:2]), True)
             c = c[2:]
         print("Waiting for data")
@@ -111,8 +113,8 @@ async def run(address, loop):
         await asyncio.sleep(50.0,loop)  # wait for a response
         await client.stop_notify(UUID_NORDIC_RX) 
         print("Done!")
-
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run(address, loop))
 
-### Write Code to exit the program. (Like Pressing any Key or Button to exit the Program)
+### Write Code to exit the program.
+### Puskar Will write later
